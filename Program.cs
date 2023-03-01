cea02567b90c61dcc6c11e47e3f13330cbf03d81
Thiago Bezerra de Carvalho <thiagaonaruto@gmail.com>
2023-03-01 19:54:02 -0300

    sofisticados

--- Program.cs
+++ Program.cs
@@ -1,25 +1,48 @@
 ﻿Console.Clear();
-//tentei fazer um dialogo ^^
+
+//robô
+
 Console.ForegroundColor = ConsoleColor.Yellow;
-Console.WriteLine("\nOi! Faz tempo que não te vejo Fausto!");
+Console.WriteLine("\nOi! Faz tempo que não te vejo Faustos!");
 Console.ResetColor();
+
+//usuário
 
 Console.ForegroundColor = ConsoleColor.Magenta;
 Console.Write("\nVocê errou... meu nome é...");
 string FirstNome = Console.ReadLine()!;
 Console.ResetColor();
 
+Console.ForegroundColor = ConsoleColor.DarkGray;
+Console.WriteLine("\npressione uma tecla para continuar...");
+Console.ResetColor();
+Console.ReadKey();
+
+Console.Clear();
+
+//robô
+
 Console.ForegroundColor = ConsoleColor.Yellow;
-Console.WriteLine($"\nFoi mal...{FirstNome}, eu tinha me esquecido\n Mas enfim,vamos para o objetivo!\nVocê sendo um escritor famoso tem que seguir algumas regras");
+Console.WriteLine($"\nFoi mal...{FirstNome}, eu tinha me esquecido\nMas enfim, vamos para o objetivo!\nVocê sendo um escritor famoso tem que seguir algumas regras");
 Console.WriteLine("\nE uma delas é, ter o seu **nome de catalógo**");
 Console.WriteLine("\nOkay! Seu sobrenome... por favor");
 Console.ResetColor();
 
+//usuário
+
 Console.ForegroundColor = ConsoleColor.Magenta;
 string SecondNome = Console.ReadLine()!;
-Console.WriteLine("\nE não esqueça!");
+Console.WriteLine($"\nÉ {SecondNome}, e não esqueça!");
 Console.ResetColor();
 
+Console.ForegroundColor = ConsoleColor.DarkGray;
+Console.WriteLine("\npressione uma tecla para continuar...");
+Console.ResetColor();
+Console.ReadKey();
+
+Console.Clear();
+
+//robô
 
 Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine("\nPode deixar...");
